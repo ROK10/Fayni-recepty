0fecbc6bb19ec7bf67cb1534ecc57f037aa744bd
ROK10 <rok10wot@ukr.net>
2022-05-13 23:31:26 +0300

    Frontend uprade #1

---
+++ src/main/java/com/naukma/faynirecepty/FayniReceptyApplication.java
@@ -0,0 +1,13 @@
+package com.naukma.faynirecepty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FayniReceptyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FayniReceptyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naukma/faynirecepty/FayniReceptyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naukma.faynirecepty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FayniReceptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
