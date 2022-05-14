7b84309445953b9c2de54cae136e267109b8b37f
daria-zubritska <77271382+daria-zubritska@users.noreply.github.com>
2022-05-14 16:30:16 +0300

    Entities and repositories added

--- src/main/java/com/naukma/faynirecepty/repository/IngredientRepository.java
+++ src/main/java/com/naukma/faynirecepty/repository/IngredientRepository.java
@@ -2,9 +2,11 @@ package com.naukma.faynirecepty.repository;
 
 import com.naukma.faynirecepty.model.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository("ingredientRepository")
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
     List<Ingredient> findAll();
--- src/main/java/com/naukma/faynirecepty/repository/RecipeRepository.java
+++ src/main/java/com/naukma/faynirecepty/repository/RecipeRepository.java
@@ -2,10 +2,12 @@ package com.naukma.faynirecepty.repository;
 
 import com.naukma.faynirecepty.model.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository("recipeRepository")
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     Optional<Recipe> findById(Long id);
